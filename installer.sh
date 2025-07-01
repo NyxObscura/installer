f887ccf4db06f85da3943442b7d6b25d121d6107
NyxObscura <service@obscuraworks.com>
2025-07-01 16:00:07 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,98 +1,51 @@
 #!/bin/bash
 
-set -e
+# =================================================================
+# Installer Loader oleh NyxObscura
+# Skrip ini memverifikasi integritas sebelum eksekusi.
+# =================================================================
 
-######################################################################################
-#                                                                                    #
-# Project 'pterodactyl-installer'                                                    #
-#                                                                                    #
-# Copyright (C) 2018 - 2025, Vilhelm Prytz, <vilhelm@prytznet.se>                    #
-#                                                                                    #
-# https://github.com/pterodactyl-installer/pterodactyl-installer                     #
-#                                                                                    #
-######################################################################################
+# --- KONFIGURASI ---
 
-export GITHUB_SOURCE="v1.1.1"
-export SCRIPT_RELEASE="v1.1.1"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"
+SCRIPT_URL="https://raw.githubusercontent.com/NyxObscura/installer/main/obsfuscated.sh"
 
-LOG_PATH="/var/log/pterodactyl-installer.log"
 
-# check for curl
-if ! [ -x "$(command -v curl)" ]; then
-  echo "* curl is required in order for this script to work."
-  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
-  exit 1
+EXPECTED_HASH="99f0e300f89f9ea5b1d35560e0f2c2af0989da9423294bb7244dec235bcf2be7" # <-- GANTI INI
+
+# --- VARIABEL WARNA ---
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+BOLD='\033[1m'
+RESET='\033[0m'
+
+# --- LOGIKA UTAMA ---
+echo -e "${CYAN}${BOLD}Mempersiapkan installer...${RESET}"
+
+
+SCRIPT_CONTENT=$(curl -sL "$SCRIPT_URL")
+
+
+if [ -z "$SCRIPT_CONTENT" ]; then
+    echo -e "${RED}${BOLD}Gagal mengunduh skrip installer. Periksa koneksi internet atau URL.${RESET}"
+    exit 1
 fi
 
-# Always remove lib.sh, before downloading it
-[ -f /tmp/lib.sh ] && rm -rf /tmp/lib.sh
-curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/master/lib/lib.sh
-# shellcheck source=lib/lib.sh
-source /tmp/lib.sh
 
-execute() {
-  echo -e "\n\n* pterodactyl-installer $(date) \n\n" >>$LOG_PATH
+ACTUAL_HASH=$(echo -n "$SCRIPT_CONTENT" | sha256sum | awk '{print $1}')
 
-  [[ "$1" == *"canary"* ]] && export GITHUB_SOURCE="master" && export SCRIPT_RELEASE="canary"
-  update_lib_source
-  run_ui "${1//_canary/}" |& tee -a $LOG_PATH
 
-  if [[ -n $2 ]]; then
-    echo -e -n "* Installation of $1 completed. Do you want to proceed to $2 installation? (y/N): "
-    read -r CONFIRM
-    if [[ "$CONFIRM" =~ [Yy] ]]; then
-      execute "$2"
-    else
-      error "Installation of $2 aborted."
-      exit 1
-    fi
-  fi
-}
-
-welcome ""
-
-done=false
-while [ "$done" == false ]; do
-  options=(
-    "Install the panel"
-    "Install Wings"
-    "Install both [0] and [1] on the same machine (wings script runs after panel)"
-    # "Uninstall panel or wings\n"
-
-    "Install panel with canary version of the script (the versions that lives in master, may be broken!)"
-    "Install Wings with canary version of the script (the versions that lives in master, may be broken!)"
-    "Install both [3] and [4] on the same machine (wings script runs after panel)"
-    "Uninstall panel or wings with canary version of the script (the versions that lives in master, may be broken!)"
-  )
-
-  actions=(
-    "panel"
-    "wings"
-    "panel;wings"
-    # "uninstall"
-
-    "panel_canary"
-    "wings_canary"
-    "panel_canary;wings_canary"
-    "uninstall_canary"
-  )
-
-  output "What would you like to do?"
-
-  for i in "${!options[@]}"; do
-    output "[$i] ${options[$i]}"
-  done
-
-  echo -n "* Input 0-$((${#actions[@]} - 1)): "
-  read -r action
-
-  [ -z "$action" ] && error "Input is required" && continue
-
-  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
-  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
-  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
-done
-
-# Remove lib.sh, so next time the script is run the, newest version is downloaded.
-rm -rf /tmp/lib.sh
+if [[ "$ACTUAL_HASH" == "$EXPECTED_HASH" ]]; then
+    echo -e "${GREEN}Verifikasi integritas berhasil.${RESET}"
+    echo -e "${CYAN}Menjalankan skrip...${RESET}"
+    echo 
+    
+    bash <(echo "$SCRIPT_CONTENT")
+else
+    echo -e "${RED}${BOLD}==================== PERINGATAN KEAMANAN ====================${RESET}"
+    echo -e "${RED}Hash skrip tidak cocok! Eksekusi dibatalkan.${RESET}"
+    echo -e "Ini bisa berarti skrip telah diubah atau rusak saat diunduh."
+    echo -e "Hash yang Diharapkan: ${GREEN}$EXPECTED_HASH${RESET}"
+    echo -e "Hash Aktual         : ${RED}$ACTUAL_HASH${RESET}"
+    exit 1
+fi
